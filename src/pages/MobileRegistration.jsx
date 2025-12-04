ada9784907a8f213e4ff78e9a93603eebbd8a1b0
Sakshi Sen <sakshi_sen@ildc-inlp.idealake.com>
2025-12-04 11:37:11 +0530

    added the function for the mobile registration form to store in localstorage.

--- src/pages/MobileRegistration.jsx
+++ src/pages/MobileRegistration.jsx
@@ -1,8 +1,48 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const MobileRegistration = () => {
+  const navigate = useNavigate();
+  const [mobile, setMobile] = useState("")
+
+  //load saved value
+  useEffect(()=>{
+    const saved = localStorage.getItem("mobile");
+    if(saved) setMobile(saved)
+  },[])
+
+  //save value automatically
+  useEffect(()=>{
+    localStorage.setItem("mobile",mobile)
+  },[mobile])
+
+
+  const handleSubmit=(e)=>{
+    e.preventDefault()
+
+    if (!/^[6-9]\d{9}$/.test(mobile)) {
+      alert("Please enter a valid 10-digit mobile number.");
+      return;
+    }
+
+    navigate("/verify-otp")
+  }
+
   return (
-    <div>MobileRegistration</div>
+    <main className='mobile-container'>
+      <h1 className='title'>Mobile Registration</h1>
+
+      <form onSubmit={handleSubmit} noValidate>
+        <label htmlFor='mobile'>Enter your mobile number</label>
+
+        <input type='tel' id='mobile' aria-label='Mobile Number'
+          placeholder='8774563986' maxLength={10} value={mobile}
+          onChange={(e) => setMobile(e.target.value.replace(/\D/g, ""))}
+          required />
+
+        <button type='submit' className='btn-primary'>Send OTP</button>  
+      </form>
+    </main>
   )
 }
 
