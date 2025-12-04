1b3c47c2c85daca522b7b09a55ebab6407c29aa4
Sakshi Sen <sakshi_sen@ildc-inlp.idealake.com>
2025-12-04 11:43:06 +0530

    Step 1: Mobile Registration completed

--- src/pages/MobileRegistration.jsx
+++ src/pages/MobileRegistration.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
-
+import '../styles/mobileRegistration.scss'
 const MobileRegistration = () => {
   const navigate = useNavigate();
   const [mobile, setMobile] = useState("")
